d6fad3ff980be2e7c13ed9e3e05b62e984c9caa4
Adrian Imboden <mail@adrianimboden.ch>
2018-05-14 13:45:11 +0200

    change to stateful runner

--- dojo/first/cyber-dojo.sh
+++ dojo/first/cyber-dojo.sh
@@ -1,11 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir -p ${HOME}/build
-cd ${HOME}/build
+mkdir -p ${DIR}/build
+cd ${DIR}/build
 bash -c "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 $(cat /prebuilt/inst/debug/cmake_flags) ${DIR}"
-cmake --build .
+cmake --build . -- -j4
 
 #for file in $(find ${DIR}/*/ -iname *.cpp); do
 #	clang-tidy ${file} -p ${HOME}/build -quiet 2>/dev/null | grep -v "warnings generated." &
